c06ce07439c2fea2b4fddb918768ad568f9f438e
soovinchoi <soovinnnnnn@gmail.com>
2021-03-15 14:57:38 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,9 +66,20 @@ let randomIndex2;
 //   inconsolata = loadFont('assets/inconsolata.otf');
 // }
 
+let animating = false;
+
 function setup() {
   createCanvas(400, 400);
   background(238,130,238);
+  textSize(12);
+
+  text("click to find your destiny moment of bumping into someone", 50, 50);
+
+  // setTimeout(changeBackground, 1000);
+  // setInterval(changeBackground, 1000);
+  //function copy and past not
+
+
 
   // console.log("initial array is");
   // console.log(names);
@@ -94,25 +105,64 @@ function setup() {
 function draw() {
   //text('click to find your destiny moment of bumping into someone', 45, 200);
 
+
+
+if(animating == true){
+  ellipse(random(width), random(height), random(50, 200));
+}
+}
+
+// function changeBackground(){
+//   if (counter <= 5){
+//   counter++;
+//   console.log(counter);
+//   background(random(255),random(255), random(255));
+//   setTimeout(changeBackground, 1000);
+// } else {
+//
+// }
+//
+// }
+
+function randomizer(){
+  animating = false;
+
+  if (names[0]){
+
+  //this displays random name and splices it out of array
+    background(random(240,150,238));
+
+  //names random
+    randomIndex = int(random(names.length));
+    randomIndex2 = int(random(words.length));
+    textStyle(NORMAL);
+    textFont('Orbitron');
+    textAlign(CENTER, CENTER);
+    // text('${names[randomIndex].name} says...', 180, 150);
+
+    text(names[randomIndex].name + " says...", 180, 150);
+
+  //words random
+    textStyle(ITALIC);
+    textFont('Homemade Apple');
+    textAlign(CENTER, CENTER);
+    text(words[randomIndex2].word, 180, 170);
+
+    names.splice(randomIndex, 1);
+  } else {
+    background(random(240,150,238));
+    text("nothing left!", 50, 50);
+
+  }
+
 }
 
 function mousePressed() {
-  background(random(240,150,238));
+  animating = true;
+  setTimeout(randomizer, 2000);
 
-//names random
-  randomIndex = int(random(names.length));
-  randomIndex2 = int(random(words.length));
-  textStyle(NORMAL);
-  textFont('Orbitron');
-  textAlign(CENTER, CENTER);
-  text(names[randomIndex].name, 180, 150);
 
-//words random
-  textStyle(ITALIC);
-  textFont('Homemade Apple');
-  textAlign(CENTER, CENTER);
-  text(words[randomIndex2].word, 180, 170);
 
-  names.splice(randomIndex, 1);
+
 
 }
